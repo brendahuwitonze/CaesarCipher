2a24c2aa38f724946ea44929d3b8c014ef66b9d2
brendahuwitonze <uwitonzebrendah@gmail.com>
2020-11-15 23:41:30 +0200

    added a decoding class

--- src/main/java/models/Caesardecrypt.java
+++ src/main/java/models/Caesardecrypt.java
@@ -1,4 +1,69 @@
 package models;
 
 public class Caesardecrypt {
+    private String text;
+    private int shiftKey;
+
+    public Caesardecrypt (String text, int shiftKey){
+        this.text=text;
+        this.shiftKey=shiftKey;
+
+    }
+
+    public int getShiftKey (){
+        return this.shiftKey;
+    }
+
+    public String getText (){
+        return this.text;
+    }
+    public static String encrypt (String text,int shiftKey){
+        if(shiftKey > 26){
+
+            shiftKey=(shiftKey%26);
+        }
+        else if(shiftKey < 0){
+
+            shiftKey=(shiftKey % 26)+26;
+        }
+        String cipherText="";
+
+        int length=text.length();
+
+        for(int i=0;i<length;i++){
+
+            char ch=text.charAt(i);
+
+            if(Character.isLetter(ch)){
+
+                if(Character.isLowerCase(ch)) {
+
+                    char c=(char) (ch + shiftKey);
+
+                    if (c > 'z') {
+                        cipherText+=(char)(ch-(26-shiftKey));
+                    }
+                    else{
+                        cipherText +=c;
+                    }
+                }
+                else if(Character.isUpperCase(ch)){
+
+                    char c=(char)(ch+shiftKey);
+
+                    if(c>'Z'){
+
+                        cipherText+=(char)(ch-(26-shiftKey));
+                    }
+                    else{
+                        cipherText +=c;
+                    }
+                }
+            }
+            else{
+                cipherText+=ch;
+            }
+        }
+        return cipherText;
+    }
 }
