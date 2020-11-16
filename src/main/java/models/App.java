565198135d79c7cad491110f3061d7308bb06699
brendahuwitonze <uwitonzebrendah@gmail.com>
2020-11-16 02:23:38 +0200

    declared some variables

--- src/main/java/models/App.java
+++ src/main/java/models/App.java
@@ -1,4 +1,18 @@
 package models;
-
+import java.io.Console;
 public class App {
+    public static void main(String[]orgs){
+        Console myConsole=System.console();
+        System.out.println ("Enter the text");
+        String word=myConsole.readLine ();
+        System.out.println ("Enter the key");
+        String key=myConsole.readLine ();
+        int nKey=Integer.parseInt (key);
+        if((nKey<1)||(nKey>26)){
+
+        }
+
+    }
+
+
 }
