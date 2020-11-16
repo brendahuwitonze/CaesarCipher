d08dedadcbc09de0364956793200dd2427348cbf
brendahuwitonze <uwitonzebrendah@gmail.com>
2020-11-16 07:03:58 +0200

    added a funtion

--- src/main/java/models/Cipher.java
+++ src/main/java/models/Cipher.java
@@ -1,6 +1,6 @@
 package models;
 import java.io.Console;
-public class App {
+public class Cipher {
     public static void main(String[]orgs){
         Console myConsole=System.console();
         System.out.println ("Enter the text");
@@ -8,9 +8,12 @@ public class App {
         System.out.println ("Enter the key");
         String key=myConsole.readLine ();
         int nKey=Integer.parseInt (key);
-        if((nKey<1)||(nKey>26)){
-
+        if((nKey<1)||(nKey>26)) {
+            System.out.println ("Enter the key that is between 1 and 26");
         }
+        Caesar result=new  Caesar(word,nKey);
+        String output=result.encrypt (word,nKey);
+        System.out.println ("The encrypted word is"+" "+ output);
 
     }
 
